420479002a8a76707e8a53e80ce9459525758331
UdonDa <udooon0727@gmail.com>
2017-06-03 14:37:31 +0900

    jsonをPOSTできた！

--- app/src/main/java/com/example/wally_nagama/paripigrass/HttpPostTask.java
+++ app/src/main/java/com/example/wally_nagama/paripigrass/HttpPostTask.java
@@ -1,10 +1,19 @@
 package com.example.wally_nagama.paripigrass;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -12,12 +21,58 @@ import java.net.URL;
  */
 
 public class HttpPostTask extends AsyncTask<URL, Void, Void> {
+    final String json = "{\"events\":{\"name\":\"田中\",\"score\":3000}}";
+            /*
+            "{\"events\":{" +
+                    "\"name\":\"name1\"," +
+                    "\"score\":5000" +
+                    "}}";
+*/
+
+
     @Override
     protected Void doInBackground(URL... urls) {
-
-        final URL url = urls[0];
-        HttpURLConnection con = null;
         try {
+            String buffer = "";
+            HttpURLConnection con = null;
+            URL url = new URL("http://invita.tech:26000/callback");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setInstanceFollowRedirects(false);
+            con.setRequestProperty("Accept-Language", "jp");
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            OutputStream os = con.getOutputStream();
+            PrintStream ps = new PrintStream(os);
+            ps.print(json);
+            ps.close();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+            buffer = reader.readLine();
+
+            JSONArray jsonArray = new JSONArray(buffer);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                Log.d("HTTP REQ", jsonObject.getString("name"));
+            }
+            con.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+
+
+/*      try {
             con = (HttpURLConnection) url.openConnection();
             con.setDoOutput(true);
             con.setChunkedStreamingMode(0);
@@ -44,7 +99,6 @@ public class HttpPostTask extends AsyncTask<URL, Void, Void> {
                 // 正常
                 // レスポンス取得処理を実行
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -52,7 +106,6 @@ public class HttpPostTask extends AsyncTask<URL, Void, Void> {
                 con.disconnect();
             }
         }
-        return null;
-    }
+*/
 
-}
+//        return null;
